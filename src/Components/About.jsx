e34d3e097c3da8d9d46f0984f604fb6b586ca1ce
Abdul Basit <72499151+Basitx72@users.noreply.github.com>
2023-05-01 21:36:09 +0000

    about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,6 @@
-/**
- * About component
- *
- * Space for you to describe more about yourself.
- */
-
 import React from "react";
 import image from "../images/motion-background.jpg";
+
 const imageAltText = "purple and blue abstract background";
 const description = "Professional with a passion for design and a commitment to excellence.";
 const skillsList = [
@@ -17,37 +12,22 @@ const skillsList = [
   "Graphic design",
 ];
 const detailOrQuote = "Embrace the 10X mindset, and success will follow. - Grant Cardone";
+
 const About = () => {
   return (
     <section className="padding" id="about">
       <img className="background" src={image} alt={imageAltText} />
-      <div
-        style={{
-          backgroundColor: "white",
-          width: "50%",
-          padding: "4rem",
-          margin: "3rem auto",
-          textAlign: "center",
-        }}
-      >
+      <div className="about-content">
         <h2>About Myself</h2>
         <p className="large">{description}</p>
         <hr />
-        <ul
-          style={{
-            textAlign: "left",
-            columns: 2,
-            fontSize: "1.25rem",
-            margin: "2rem 3rem",
-            gap: "3rem",
-          }}
-        >
+        <ul className="skills-list">
           {skillsList.map((skill) => (
             <li key={skill}>{skill}</li>
           ))}
         </ul>
         <hr />
-        <p style={{ padding: "1rem 3rem 0" }}>{detailOrQuote}</p>
+        <p className="quote">{detailOrQuote}</p>
       </div>
     </section>
   );
